bb022686457bf56684f94ad75949d5e53b698c60
lily <liyan@gbase.cn>
2017-03-13 17:02:00 +0800

    print "登录成功"

--- autosjg/src/test_case/test_home.py
+++ autosjg/src/test_case/test_home.py
@@ -6,19 +6,14 @@ from autosjg.src.login import Login
 # 这里用例直接继承了login.py中的类，继承过来后直接写case,也可以调用公共方法
 class Testlogin(Login):
 
-# 这里用例直接继承了前面的类，继承过来后直接写case,也可以调用公共方法
-class Test_home(Login):
     def test_01(self):
-        # 个人主页博客地址测试
-        self.login("liyan@gbase.cn", "111111")
-
         # 登录之后的case
-        # 获取个人主页上的特征文字
+        # 获取个人主页上的特征文字做验证对比
         text = self.driver.find_element_by_class_name("userguide").text
         print text
         self.assertEqual(text, u"使用引导")
+        print "登录成功"
 
     # def test_02(self):
-    #     self.login("liyan@gbase.cn", "111111")
     #     # 登录之后的case
     #     # 后面的case自由发挥吧
