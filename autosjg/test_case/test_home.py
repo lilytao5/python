24bd329fca2cccb19050ddd228d7405ceecedb11
lily <liyan@gbase.cn>
2017-03-20 11:16:07 +0800

    调整目录结构

--- autosjg/test_case/test_home.py
+++ autosjg/test_case/test_home.py
@@ -1,7 +1,6 @@
 # coding:utf-8
 # 从login模块导入Login类
-import time
-from autosjg.src.index import Login
+from autosjg.index import Login
 
 # 这里用例直接继承了login.py中的类，继承过来后直接写case,也可以调用公共方法
 class Testlogin(Login):
